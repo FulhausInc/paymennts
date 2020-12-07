5628b7ec63cf14a4b1977836e2faa05fba74bf99
Alan-eMartin <alan.edward.martin@gmail.com>
2020-12-07 13:32:23 -0500

    Added: Styling, classNames to fields, media-queries | Changes: Destructured props, landing-page-wrapper

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,64 +1,71 @@
-import React, { useState, useEffect } from "react";
-import fetchUtil from "../../Functions/fetchUtils";
-import "./LandingPage.scss";
-import { connect } from "react-redux";
-import { BlockLoading } from "react-loadingg";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState, useEffect, allPaymentDetails } from 'react'
+import fetchUtil from '../../Functions/fetchUtils'
+import './LandingPage.scss'
+import { connect } from 'react-redux'
+import { BlockLoading } from 'react-loadingg'
+import { v4 as uuidv4 } from 'uuid'
 
-const UnconnectedLandingPage = (props) => {
-  const [isLoading, setIsLoading] = useState(true);
+const UnconnectedLandingPage = ({ history, dispatch, allPaymentDetails }) => {
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
     const fetchAllPaymentDetails = async () => {
-      let response = await fetchUtil("/payments/details", "GET", "");
+      let response = await fetchUtil('/payments/details', 'GET', '')
 
       if (response.success) {
-        props.dispatch({
-          type: "set-allPaymentDetails",
+        dispatch({
+          type: 'set-allPaymentDetails',
           value: response.data,
-        });
+        })
 
-        console.log(response.data);
-        setIsLoading(false);
+        console.log(response.data)
+        // console.log(response.data)
+        setIsLoading(false)
       } else {
-        console.log(response.message);
-        setIsLoading(false);
+        console.log(response.message)
+        setIsLoading(false)
       }
-    };
+    }
 
-    fetchAllPaymentDetails();
-  }, []);
+    fetchAllPaymentDetails()
+  }, [])
 
   return isLoading ? (
-    <div className="quote-preview-wrapper-loader">
-      <BlockLoading color="#FF4E24" />
+    <div className='quote-preview-wrapper-loader'>
+      <BlockLoading color='#FF4E24' />
     </div>
   ) : (
-    <div className="landing-page-wrapper">
+    <section className='payments-landing-page'>
       <h1>All Payments</h1>
-      {props.allPaymentDetails.map((paymentDetails) => {
-        return (
-          <div key={uuidv4()} className="landing-page-payment">
-            <div
-              onClick={() =>
-                props.history.push("/generate?pid=" + paymentDetails.paymentID)
-              }
-            >
-              {paymentDetails.paymentID}
-            </div>
-            <p>{paymentDetails.payerName}</p>
-            <p>{paymentDetails.description}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+      <div className='landing-page-wrapper'>
+        <table className='landing-page-payments-container'>
+          {allPaymentDetails.map((paymentDetails) => {
+            return (
+              <div key={uuidv4()} className='landing-page-payment'>
+                <div
+                  className='payment-id'
+                  title={`View Payment Details`}
+                  onClick={() =>
+                    history.push('/generate?pid=' + paymentDetails.paymentID)
+                  }
+                >
+                  {paymentDetails.paymentID}
+                </div>
+                <p className='payee-name'>{paymentDetails.payerName}</p>
+                <p className='payment-details'>{paymentDetails.description}</p>
+              </div>
+            )
+          })}
+        </table>
+      </div>
+    </section>
+  )
+}
 
 let mapStateToProps = (state) => {
   return {
     allPaymentDetails: state.allPaymentDetails,
-  };
-};
+  }
+}
 
-let LandingPage = connect(mapStateToProps)(UnconnectedLandingPage);
-export default LandingPage;
+let LandingPage = connect(mapStateToProps)(UnconnectedLandingPage)
+export default LandingPage
